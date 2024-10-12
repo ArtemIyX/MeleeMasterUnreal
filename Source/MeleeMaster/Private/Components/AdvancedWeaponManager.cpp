4b6f8c1a3b608d907b516f5ade028c22919c6c31
Wellsaik <artem.podorozhko@outlook.com>
2024-10-12 22:08:03 +0300

    Status variables for manager

--- Source/MeleeMaster/Private/Components/AdvancedWeaponManager.cpp
+++ Source/MeleeMaster/Private/Components/AdvancedWeaponManager.cpp
@@ -3,6 +3,11 @@
 
 #include "Components/AdvancedWeaponManager.h"
 
+#include "Engine/ActorChannel.h"
+#include "Net/UnrealNetwork.h"
+#include "Net/Core/PushModel/PushModel.h"
+#include "Objects/AbstractWeapon.h"
+
 
 // Sets default values for this component's properties
 UAdvancedWeaponManager::UAdvancedWeaponManager()
@@ -15,12 +20,56 @@ UAdvancedWeaponManager::UAdvancedWeaponManager()
 }
 
 
+void UAdvancedWeaponManager::SetCurrentWeaponPtr(UAbstractWeapon* InNewWeapon)
+{
+	this->CurrentWeapon = InNewWeapon;
+	MARK_PROPERTY_DIRTY_FROM_NAME(UAdvancedWeaponManager, CurrentWeapon, this);
+}
+
+void UAdvancedWeaponManager::SetManagingStatus(EWeaponManagingStatus InStatus)
+{
+	this->ManagingStatus = InStatus;
+	MARK_PROPERTY_DIRTY_FROM_NAME(UAdvancedWeaponManager, ManagingStatus, this);
+}
+
+void UAdvancedWeaponManager::SetFightingStatus(EWeaponFightingStatus InStatus)
+{
+	this->FightingStatus = InStatus;
+	MARK_PROPERTY_DIRTY_FROM_NAME(UAdvancedWeaponManager, FightingStatus, this);
+}
+
+void UAdvancedWeaponManager::SetDirection(EWeaponDirection InDirection)
+{
+	this->CurrentDirection = InDirection;
+	MARK_PROPERTY_DIRTY_FROM_NAME(UAdvancedWeaponManager, CurrentDirection, this);
+}
+
 // Called when the game starts
 void UAdvancedWeaponManager::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
+}
+
+void UAdvancedWeaponManager::OnRep_CurrentWeapon()
+{
+}
+
+void UAdvancedWeaponManager::OnRep_WeaponList()
+{
+}
+
+void UAdvancedWeaponManager::OnRep_ManagingStatus()
+{
+}
+
+void UAdvancedWeaponManager::OnRep_FightingStatus()
+{
+}
+
+void UAdvancedWeaponManager::OnRep_CurrentDirection()
+{
 }
 
 
@@ -31,4 +80,39 @@ void UAdvancedWeaponManager::TickComponent(float DeltaTime, ELevelTick TickType,
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+bool UAdvancedWeaponManager::ReplicateSubobjects(UActorChannel* Channel, FOutBunch* Bunch, FReplicationFlags* RepFlags)
+{
+	bool sup = Super::ReplicateSubobjects(Channel, Bunch, RepFlags);
+
+	if (IsValid(CurrentWeapon))
+	{
+		sup |= Channel->ReplicateSubobject(CurrentWeapon, *Bunch, *RepFlags);
+		sup |= CurrentWeapon->ReplicateSubobjects(Channel, Bunch, RepFlags);
+	}
+	if (WeaponList.Num() > 0)
+	{
+		for (UAbstractWeapon* el : WeaponList)
+		{
+			if (IsValid(el))
+			{
+				sup |= Channel->ReplicateSubobject(el, *Bunch, *RepFlags);
+				sup |= el->ReplicateSubobjects(Channel, Bunch, RepFlags);
+			}
+		}
+	}
+	return sup;
+}
+
+void UAdvancedWeaponManager::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	FDoRepLifetimeParams Params;
+	Params.bIsPushBased = true;
+	DOREPLIFETIME_WITH_PARAMS_FAST(UAdvancedWeaponManager, CurrentWeapon, Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(UAdvancedWeaponManager, WeaponList, Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(UAdvancedWeaponManager, ManagingStatus, Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(UAdvancedWeaponManager, FightingStatus, Params);
+	DOREPLIFETIME_WITH_PARAMS_FAST(UAdvancedWeaponManager, CurrentWeapon, Params);
 }
--- Source/MeleeMaster/Public/Components/AdvancedWeaponManager.h
+++ Source/MeleeMaster/Public/Components/AdvancedWeaponManager.h
@@ -7,6 +7,42 @@
 #include "AdvancedWeaponManager.generated.h"
 
 
+class UAbstractWeapon;
+class UWeaponDataAsset;
+
+UENUM(Blueprintable, BlueprintType)
+enum class EWeaponManagingStatus : uint8
+{
+	Idle, // No actions, can start to equip another weapon
+	Equipping, // Equipping new weapon 
+	DeEquipping, // DeEquipping current weapon
+	Busy, // We are in fight, using special attack, stunned etc...
+	NoWeapon, // WTF ??
+};
+
+UENUM(Blueprintable, BlueprintType)
+enum class EWeaponFightingStatus : uint8
+{
+	Idle, // No actions, ready to fight
+	Charging, // Left click is holding
+	Blocking, // Right click is holding
+	PostBlock, // Cooldown after blocked
+	PostAttack, // Cooldown after attacked
+	HighStunned, // Faced with a maximally charged enemy attack
+	SuccessfullyPaired, // Successfully sparred and ready to counter attack
+	ParryStunned, // Was sparred, causing him to be slowed and stunned
+	Busy, // Jumping, Dashing, Stunned etc..
+};
+
+UENUM(Blueprintable, BlueprintType)
+enum class EWeaponDirection : uint8
+{
+	Forward,
+	Backward,
+	Right,
+	Left
+};
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class MELEEMASTER_API UAdvancedWeaponManager : public UActorComponent
 {
@@ -17,11 +53,73 @@ public:
 	UAdvancedWeaponManager();
 
 protected:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_CurrentWeapon)
+	UAbstractWeapon* CurrentWeapon;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_WeaponList)
+	TArray<UAbstractWeapon*> WeaponList;
+
+	// Equipping status
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_ManagingStatus)
+	EWeaponManagingStatus ManagingStatus;
+
+	// Block, Attack etc..
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_FightingStatus)
+	EWeaponFightingStatus FightingStatus;
+
+	// Block, attack and parry direction
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_CurrentDirection)
+	EWeaponDirection CurrentDirection;
+
+public:
+
+
+protected:
+	virtual void SetCurrentWeaponPtr(UAbstractWeapon* InNewWeapon);
+	virtual void SetManagingStatus(EWeaponManagingStatus InStatus);
+	virtual void SetFightingStatus(EWeaponFightingStatus InStatus);
+	virtual void SetDirection(EWeaponDirection InDirection);
+
+protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+protected:
+	UFUNCTION()
+	virtual void OnRep_CurrentWeapon();
+
+	UFUNCTION()
+	virtual void OnRep_WeaponList();
+
+	UFUNCTION()
+	virtual void OnRep_ManagingStatus();
+
+	UFUNCTION()
+	virtual void OnRep_FightingStatus();
+
+	UFUNCTION()
+	virtual void OnRep_CurrentDirection();
 
 public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
+	virtual bool ReplicateSubobjects(UActorChannel* Channel, FOutBunch* Bunch, FReplicationFlags* RepFlags) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="AdvancedWeaponManager|Getters")
+	FORCEINLINE UAbstractWeapon* GetCurrentWeapon() const { return CurrentWeapon; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="AdvancedWeaponManager|Getters")
+	TArray<UAbstractWeapon*> GetWeaponList() const { return WeaponList; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="AdvancedWeaponManager|Getters")
+	FORCEINLINE EWeaponManagingStatus GetManagingStatus() const { return ManagingStatus; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="AdvancedWeaponManager|Getters")
+	FORCEINLINE EWeaponFightingStatus GetFightingStatus() const { return FightingStatus; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="AdvancedWeaponManager|Getters")
+	FORCEINLINE EWeaponDirection GetCurrentDirection() const { return CurrentDirection; }
 };
